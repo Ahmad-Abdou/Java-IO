d2b163a387d3b0802ee2317d91f85e53323767d0
Ahmad-Abdou <kyoub1996@gmail.com>
2021-02-13 18:36:51 +0100

    Exercise 6 Assignment is done.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,30 +1,31 @@
 package org.example;
 
-import Util.CSV;
+import IO.CSV;
+import JsonIO.JsonIO;
 import org.example.model.Car;
+import org.example.model.Owner;
 
 import java.io.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) {
+public class App {
+    public static void main(String[] args) {
         CSV cs = new CSV();
 
 
-        File toRead = new File("C:\\Users\\kyoub\\IdeaProjects\\Java I\\Java-IO\\Read-Exercise1.txt") ;
+        File toRead = new File("C:\\Users\\kyoub\\IdeaProjects\\Java I\\Java-IO\\Read-Exercise1.txt");
         System.out.println("Exercise 1 :");
         String str = cs.read(toRead);
 //        System.out.println(str);
         System.out.println("----------------------------------------------------------------------------------");
         System.out.println("Exercise 2 :");
-        File ReadingNames =new File("C:\\Users\\kyoub\\IdeaProjects\\Java I\\Java-IO\\Names.txt");
+        File ReadingNames = new File("C:\\Users\\kyoub\\IdeaProjects\\Java I\\Java-IO\\Names.txt");
 //        System.out.println(cs.read2(ReadingNames));
         System.out.println("----------------------------------------------------------------------------------");
         System.out.println("Exercise 3 :");
@@ -35,26 +36,34 @@ public class App
         list.add("Jack");
         list.add("Gorge");
         File destination = new File("C:\\Users\\kyoub\\IdeaProjects\\Java I\\Java-IO\\Exercise3.txt");
-        cs.write(destination,list);
+        cs.write(destination, list);
         System.out.println("----------------------------------------------------------------------------------");
         System.out.println("Exercise 4 :");
         File copyDestination = new File("C:\\Users\\kyoub\\IdeaProjects\\Java I\\Java-IO\\FolderDestination");
         File copySource = new File("C:\\Users\\kyoub\\IdeaProjects\\Java I\\Java-IO\\FileToCopy");
-        cs.copy(copyDestination,copySource);
+        cs.copy(copyDestination, copySource);
         System.out.println("----------------------------------------------------------------------------------");
         System.out.println("Exercise 5 :");
         List<Car> cars = new ArrayList<>();
-        cars.add(new Car(12345,"Bmw",2007));
-        cars.add(new Car(46457,"Audi",2009));
-        cars.add(new Car(87643,"Ford",2011));
-
-        CSV.SavingToFile(cars,"carList.ser");
-        List<Car>list2 =  CSV.Reading("carList.ser");
-        for(Car c : list2){
+        cars.add(new Car("JSR345", "Bmw", "R56"));
+        cars.add(new Car("HSU457", "Audi", "Y4"));
+        cars.add(new Car("ORT643", "Ford", "T8"));
+        CSV.SavingToFile(cars, "carList.ser");
+        List<Car> list2 = CSV.Reading("carList.ser");
+        for (Car c : list2) {
             System.out.println(c.toString());
         }
-
-
+        System.out.println("----------------------------------------------------------------------------------");
+        System.out.println("Exercise 6 :");
+        List<Car> cars2 = new ArrayList<>();
+        cars2.add(new Car("HFW125", "BMW", "T35", LocalDate.parse("2010-05-09"),
+                new Owner("Simon", LocalDate.parse("1980-05-06"))));
+        cars2.add(new Car("KFT736", "Audi", "R8", LocalDate.parse("2012-02-11"),
+                new Owner("Jack", LocalDate.parse("1987-10-09"))));
+        JsonIO jsonIO = new JsonIO();
+        jsonIO.serializeToJson(cars2, new File("cars.json"));
+        jsonIO.deserialize(new File("cars.json"));
+        cars2.forEach(System.out::println);
 
 
     }
